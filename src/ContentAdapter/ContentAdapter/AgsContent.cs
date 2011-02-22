f1e17f99984ce2697a3661656ba86890b67c3d7c
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-02-22 17:02:33 +0100

    Implementierungs-Projekte hinzu

---
+++ src/ContentAdapter/ContentAdapter/AgsContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+using EsriDE.Samples.ContentFinder.DomainModel;
+
+namespace EsriDE.Samples.ContentFinder.ContentAdapter
+{
+	public class AgsContent : Content
+	{
+		public AgsContent(string title, Bitmap bitmap, Uri uri) : base(title, bitmap, uri)
+		{
+		}
+	}
+}
---
+++ src/ContentAdapter/ContentAdapter/MxdContent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using EsriDE.Samples.ContentFinder.DomainModel;
+
+namespace EsriDE.Samples.ContentFinder.ContentAdapter
+{
+	public class MxdContent : Content
+	{
+		public string Subject { get; private set; }
+		public string Comments { get; private set; }
+
+		public MxdContent(string title, Bitmap bitmap, Uri uri, string subject, string comments)
+			:base(title, bitmap, uri)
+		{
+			Subject = subject;
+			Comments = comments;
+		}
+	}
+}
