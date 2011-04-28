350b2419aca4828f51abeb86e0049e9335146027
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-04-28 21:14:41 +0200

    Xml Configuration Adapter fertig

--- src/ConfigurationAdapter/XmlConfigurationAdapter/XmlConfigurationReader.cs
+++ src/ConfigurationAdapter/XmlConfigurationAdapter/XmlConfigurationReader.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 using EsriDE.Samples.ContentFinder.ConfigurationAdapter.Contract;
 using EsriDE.Samples.ContentFinder.DomainModel;
 
@@ -11,11 +13,22 @@ namespace EsriDE.Samples.ContentFinder.XmlConfigurationAdapter
 		{
 			if (null != uri)
 			{
-				//throw new NotImplementedException("Lesen mit individueller Uri ist noch nicht implementiert.");
 				throw new ApplicationException("Lesen mit individueller Uri ist noch nicht implementiert.");
 			}
 
-			yield break;
+			XDocument locationsConfig = XDocument.Load(@"Locations.config");
+
+			var r = from c in locationsConfig.Descendants("ContentToSearch")
+					select
+						new SourceBundle(c.Attribute("type").Value,
+										 from d in c.Descendants("Source")
+										 select new Source(new Uri(d.Attribute("Uri").Value),
+														   (Convert.ToBoolean(d.Attribute("IsRecursive").Value)
+																? RecursivityPolicy.Recursiv
+																: RecursivityPolicy.NotRecursiv)
+											));
+
+			return r;
 		}
 	}
 }
