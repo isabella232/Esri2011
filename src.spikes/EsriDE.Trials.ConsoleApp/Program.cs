1c7315a78e6bc0d8b061bba112c16b71112bf763
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-04-28 21:13:45 +0200

    Console-App Spike

---
+++ src.spikes/EsriDE.Trials.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EsriDE.Trials.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (0 == args.Length)
+			{
+				StartGui();
+			}
+			else
+			{
+				StartConsole();
+			}
+		}
+
+		private static void StartConsole()
+		{
+			Console.WriteLine("Console startet.");
+		}
+
+		private static void StartGui()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+	}
+}
