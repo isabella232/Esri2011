45fd2eda5adde036d95998577cc4b2eaa7c3c4b4
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-04-28 21:12:50 +0200

    Dockable Window Project-Stub

---
+++ src.spikes/EsriDE.Trials.ArcMapDockableWin/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ArcMapClassLibrary1
+{
+	[Guid("01ec3fcf-00d7-4e50-8487-bf96842d7f63")]
+	[ClassInterface(ClassInterfaceType.None)]
+	[ProgId("ArcMapClassLibrary1.Class1")]
+	public class Class1
+	{
+	}
+}
