a437439f3c690972d9692f4c7dffb56c40b912dd
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-05-10 14:24:03 +0200

    Code Cleaning

--- src/ContentAdapter/ContentAdapter.Tests/ContentLocatorResolverFixture.cs
+++ src/ContentAdapter/ContentAdapter.Tests/ContentLocatorResolverFixture.cs
@@ -1,20 +1,20 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using EsriDE.Samples.ContentFinder.ConfigurationAdapter.Contract;
 using EsriDE.Samples.ContentFinder.ContentAdapter.Contract;
-using EsriDE.Samples.ContentFinder.DomainModel;
 using NUnit.Framework;
 
 namespace EsriDE.Samples.ContentFinder.ContentAdapter.Tests
 {
+	// ReSharper disable InconsistentNaming
+
 	[TestFixture]
 	public class ContentLocatorResolverFixture
 	{
 		private IConfigurationReader _configurationReader;
 		private IEnumerable<IContentLocatorCreatorFilter> _contentLocatorCreatorFilters;
 
-		[TestFixtureSetUp]
+		[SetUp]
 		private void Setup()
 		{
 			_configurationReader = new SampleConfigurationReader();
@@ -30,48 +30,23 @@ namespace EsriDE.Samples.ContentFinder.ContentAdapter.Tests
 		[Test]
 		public void ResolvingContentLocators_WithValidInput_Returns3ContentLocators()
 		{
+			// siehe SampleConfigurationReader
 			var sourceBundles = _configurationReader.ReadConfiguration(null);
 			IContentLocatorResolver resolver = new ContentLocatorResolver(_contentLocatorCreatorFilters);
+
+			// einzelne Auflösung testen
 			foreach (var sourceBundle in sourceBundles)
 			{
 				var locator = resolver.ResolveContentLocator(sourceBundle);
 				Assert.That(locator, !Is.Null);
 			}
 
-
+			// Auflösung als Liste testen
 			var locators = resolver.ResolveContentLocators(sourceBundles);
 			Assert.That(locators, !Is.Null);
 			Assert.That(locators.ToList().Count, Is.EqualTo(3));
 		}
 
-		[Test]
-		public void Do()
-		{
-			var configurations = _configurationReader.ReadConfiguration(null);
-			var adapters = configurations.GetAdapters();
-
-			foreach (var adapter in adapters)
-			{
-				adapter.FoundContent += ProcessContent;
-				adapter.StartSearch();
-			}
-
-		}
-
-		private void ProcessContent(Content content)
-		{
-			Console.WriteLine(content.Title);
-		}
-	}
-
-	public static class ConfigurationExtensions
-	{
-		public static IEnumerable<IContentLocator> GetAdapters(this IEnumerable<SourceBundle> sourceBundles)
-		{
-			//var contentLocatorCreatorResolver = new ContentLocatorCreatorResolver();
-			//var contentLocators = contentLocatorCreatorResolver.GetContentLocators(sourceBundles);
-			//return contentLocators;
-			yield break;
-		}
+		// ReSharper restore InconsistentNaming
 	}
 }
