fa09f0dbc3779dc0acf66c99418c269bbe86b45b
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-05-10 13:28:36 +0200

    Codecleaning

--- src/_Fundamentals/EsriDE.Commons.Ags.Tests/MapServiceUtilFixture.cs
+++ src/_Fundamentals/EsriDE.Commons.Ags.Tests/MapServiceUtilFixture.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Text;
-using EsriDE.Commons.Ags.Contracts;
 using NUnit.Framework;
 
 //using NUnit.Framework.SyntaxHelpers;
@@ -13,12 +11,14 @@ namespace EsriDE.Commons.Ags.Tests
 	public class MapServiceUtilFixture
 	{
 		[Test]
-		public void Spike()
+		public void ConnectTo_SampleMapService_Works()
 		{
 			var uri =
 				new Uri(
 					@"http://server.arcgisonline.com/ArcGIS/rest/services/Demographics/USA_1990-2000_Population_Change/MapServer");
 			var sut = MapServiceUtil.GetMapServiceInfo(uri);
+
+			Assert.That(sut, !Is.Null, "MapService USA 1990-2000 Population konnte nicht angesprochen werden.");
 		}
 	}
 
