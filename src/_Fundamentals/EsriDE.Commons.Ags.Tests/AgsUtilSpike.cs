3e17767034f3e46b7210fb20d29a1fe0ca670f52
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-05-10 13:25:30 +0200

    Umbenennung

--- src/_Fundamentals/EsriDE.Commons.Ags.Tests/AgsUtilSpike.cs
+++ src/_Fundamentals/EsriDE.Commons.Ags.Tests/AgsUtilSpike.cs
@@ -1,56 +1,62 @@
 using System;
 using NUnit.Framework;
 
-//using NUnit.Framework.SyntaxHelpers;
-
 namespace EsriDE.Commons.Ags.Tests
 {
 	// ReSharper disable InconsistentNaming
 
 	[TestFixture]
-	public class AgsUtilFixture
+	public class AgsUtilSpike
 	{
 		[Test]
-		public void FolderErmitteln()
+		public void FindenVonFoldern()
 		{
 			var rootUri = new Uri("http://server.arcgisonline.com/ArcGIS/rest/services");
 			var folderUris = AgsUtil.GetFolderUris(rootUri);
+			var count = 0;
 			foreach (var folderUri in folderUris)
 			{
+				count++;
 				Console.WriteLine(folderUri);
 			}
 
-			//var rootUri = new Uri("http://server.arcgisonline.com/ArcGIS/rest/services/Xyz");
-			//var folderUris = AgsUtil.GetFolderUris(rootUri);
-			//foreach (var folderUri in folderUris)
-			//{
-			//    Console.WriteLine();
-			//}
+			Assert.That(count, Is.EqualTo(4), "Anzahl der gefundenen Folder (ago/root) ist unpassend.");
 		}
 
 		[Test]
-		public void ServicesErmitteln()
+		public void FindenVonServices()
 		{
+			var count = 0;
+
 			var rootUri = new Uri("http://server.arcgisonline.com/ArcGIS/rest/services");
 			var serviceUris = AgsUtil.GetServiceUris(rootUri);
 			foreach (var serviceUri in serviceUris)
 			{
+				count++;
 				Console.WriteLine(serviceUri);
 			}
+
+			Assert.That(count, Is.EqualTo(20), "Anzahl der gefundenen Services (ago/root) ist unpassend.");
 
 			rootUri = new Uri("http://server.arcgisonline.com/ArcGIS/rest/services/Demographics");
 			serviceUris = AgsUtil.GetServiceUris(rootUri);
 			foreach (var serviceUri in serviceUris)
 			{
+				count++;
 				Console.WriteLine(serviceUri);
 			}
+
+			Assert.That(count, Is.EqualTo(41), "Anzahl der gefundenen Services (ago/demographics) ist unpassend.");
 
 			rootUri = new Uri("http://vsdp1001.srvc.esri-de.com/ArcGIS/rest/services/Basemaps");
 			serviceUris = AgsUtil.GetServiceUris(rootUri);
 			foreach (var serviceUri in serviceUris)
 			{
+				count++;
 				Console.WriteLine(serviceUri);
 			}
+
+			Assert.That(count, Is.EqualTo(51), "Anzahl der gefundenen Services (demo/basemaps) ist unpassend.");
 		}
 	}
 
