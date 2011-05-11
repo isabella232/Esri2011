d5a8add414ef1ed4b684fd8c49976cca9b86e4c3
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-05-11 13:41:48 +0200

    FxCop für Warnungen vom Esri-Wizard erzeugten Code parametriert

--- src/AgdAdapter/AgdAdapter/Properties/AssemblyInfo.cs
+++ src/AgdAdapter/AgdAdapter/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("AgdAdapter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,28 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("52fe26b1-1cd4-45dd-b5bd-de3880bcacc3")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly:
+	SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Scope = "member",
+		Target = "EsriDE.Samples.ContentFinder.AgdAdapter.ArcMap.get_DockableWindowManager():ESRI.ArcGIS.Framework.IDockableWindowManager")]
+[assembly:
+	SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Scope = "member",
+		Target = "EsriDE.Samples.ContentFinder.AgdAdapter.ArcMap.get_Document():ESRI.ArcGIS.ArcMapUI.IMxDocument")]
+[assembly:
+	SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Scope = "member",
+		Target = "EsriDE.Samples.ContentFinder.AgdAdapter.ArcMap.get_Events():ESRI.ArcGIS.ArcMapUI.IDocumentEvents_Event")]
+[assembly:
+	SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Scope = "member",
+		Target = "EsriDE.Samples.ContentFinder.AgdAdapter.ArcMap.get_ThisApplication():ESRI.ArcGIS.ArcMapUI.IMxApplication")]
+[assembly:
+	SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Scope = "type",
+		Target = "EsriDE.Samples.ContentFinder.AgdAdapter.ThisAddIn")]
+[assembly:
+	SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Scope = "type",
+		Target = "EsriDE.Samples.ContentFinder.AgdAdapter.ThisAddIn+IDs")]
