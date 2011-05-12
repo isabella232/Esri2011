4b6649cfc93d899983df3a4b4e5881da90d9b289
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-05-13 00:38:32 +0200

    ...

--- src/AgdBLAdapter/AgdBLAdapter.Tests/Fixture.cs
+++ src/AgdBLAdapter/AgdBLAdapter.Tests/Fixture.cs
@@ -1,9 +1,12 @@
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
+using EsriDE.Commons.Patterns;
+using EsriDE.Samples.ContentFinder.AgdBLAdapter;
+using EsriDE.Samples.ContentFinder.ApplicationAdapter.Contract;
 using EsriDE.Samples.ContentFinder.DomainModel;
 using NUnit.Framework;
-
-//using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
+using TypeMock.ArrangeActAssert;
 
 namespace AgdBLAdapter.Tests
 {
@@ -13,10 +16,45 @@ namespace AgdBLAdapter.Tests
 	public class Fixture
 	{
 		[Test]
-		public void Do()
+		public void BuildChainOfResponsibility_Works()
 		{
 			var container = new WindsorContainer();
-			//container.Register(Component.For<ICorHandler<Content>>().ImplementedBy())
+
+			container.Register(Component.For<IChainOfResponsibilityHandler<Content>>().ImplementedBy<ApplicationMxdCorHandler>().Named("mxd").Parameters(Parameter.ForKey("successor").Eq("${ags}")));
+			container.Register(Component.For<IChainOfResponsibilityHandler<Content>>().ImplementedBy<ApplicationAgsCorHandler>().Named("ags").Parameters(Parameter.ForKey("successor").Eq("${Ende}")));
+			container.Register(Component.For<IChainOfResponsibilityHandler<Content>>().ImplementedBy<EndApplicationCorHandler>().Named("Ende"));
+
+			container.Register(Component.For<IContentProcessorAdapter>().ImplementedBy<ContentProcessorAdapter>());
+
+			var applicationAdapter = Isolate.Fake.Instance<IApplicationAdapter>();
+			container.Register(Component.For<IApplicationAdapter>().Instance(applicationAdapter));
+
+			var v = container.Resolve<IContentProcessorAdapter>();
+			Assert.That(v, Is.Not.Null);
+		}
+
+		[Test]
+		public void FakingWholeObjectTreeWithTypemock()
+		{
+			var applicationAdapter = Isolate.Fake.Instance<IApplicationAdapter>();
+			var application = applicationAdapter.Application;
+
+			Assert.That(application, Is.Not.Null);
+		}
+
+		[Test]
+		public void FakingSingleObjectsWithRhinoMocks()
+		{
+			var mockRepository = new MockRepository();
+			var applicationAdapter = mockRepository.StrictMock<IApplicationAdapter>();
+
+			With.Mocks(mockRepository)
+				.Expecting(() => SetupResult.For(applicationAdapter.Application).Return(null))
+				.Verify(delegate
+				        	{
+				        		var application = applicationAdapter.Application;
+				        		Assert.That(application, Is.Null);
+				        	});
 		}
 	}
 
--- src/AgdBLAdapter/AgdBLAdapter.Tests/ICorHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AgdBLAdapter.Tests
-{
-	public interface ICorHandler<T>
-	{
-		void Process(T data);
-		ICorHandler<T> NextLink { get; set; }
-	}
-}
