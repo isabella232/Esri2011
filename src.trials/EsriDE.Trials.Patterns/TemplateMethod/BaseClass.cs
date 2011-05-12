25668bb35d92496e6f9f3a57ff1e33b1f4cbad4d
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-05-13 00:37:41 +0200

    Patterns Bsp hinzu

---
+++ src.trials/EsriDE.Trials.Patterns/TemplateMethod/BaseClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EsriDE.Trials.Patterns.TemplateMethod
+{
+	public abstract class Abstract
+	{
+		public void TemplateMethod()
+		{
+			MacheAmAnfang();
+			MacheDies();
+			MacheJenes();
+			MacheAmEnde();
+		}
+
+		protected abstract void MacheDies();
+		protected abstract void MacheJenes();
+
+		private void MacheAmAnfang()
+		{
+			Console.WriteLine("MacheAmAnfang");
+		}
+
+		private void MacheAmEnde()
+		{
+			Console.WriteLine("MacheAmEnde");
+		}
+	}
+}
---
+++ src.trials/EsriDE.Trials.Patterns/TemplateMethod/Concrete.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EsriDE.Trials.Patterns.TemplateMethod
+{
+	public class Concrete : Abstract
+	{
+		protected override void MacheDies()
+		{
+			Console.WriteLine("MacheDies");
+		}
+
+		protected override void MacheJenes()
+		{
+			Console.WriteLine("MacheDies");
+		}
+	}
+}
