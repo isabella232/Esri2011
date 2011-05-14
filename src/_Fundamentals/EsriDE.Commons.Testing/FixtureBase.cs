9560c0929ae56815ed7f7327a1d98180b94fb8cf
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-05-14 19:17:07 +0200

    ...

---
+++ src/_Fundamentals/EsriDE.Commons.Testing/FixtureBase.cs
@@ -0,0 +1,43 @@
+using System;
+using ESRI.ArcGIS;
+using ESRI.ArcGIS.esriSystem;
+using NUnit.Framework;
+
+namespace EsriDE.Commons.Testing
+{
+	public abstract class FixtureBase
+	{
+		[TestFixtureSetUp]
+		public virtual void FixtureSetup()
+		{
+			RuntimeManager.Bind(ProductCode.EngineOrDesktop);
+			InitializeEngineLicense();
+		}
+
+		[SetUp]
+		public virtual void Setup()
+		{
+			try
+			{
+				//ContainerFacade.CreateContainer();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
+		}
+
+		private static void InitializeEngineLicense()
+		{
+			AoInitialize aoi = new AoInitializeClass();
+
+			//more license choices could be included here
+			const esriLicenseProductCode productCode = esriLicenseProductCode.esriLicenseProductCodeArcInfo;
+			if (aoi.IsProductCodeAvailable(productCode) == esriLicenseStatus.esriLicenseAvailable)
+			{
+				aoi.Initialize(productCode);
+			}
+		}
+	}
+}
