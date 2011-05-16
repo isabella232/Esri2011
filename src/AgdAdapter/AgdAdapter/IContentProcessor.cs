b1c8ae5fdcb064c9f6a01e115449f2b38569cde0
Marko Apfel, Esri Deutschland GmbH <m.apfel@esri.de>
2011-05-16 10:37:38 +0200

    Code Cleaning

--- src/AgdAdapter/AgdAdapter/IContentProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using EsriDE.Samples.ContentFinder.DomainModel;
-
-namespace EsriDE.Samples.ContentFinder.AgdAdapter
-{
-	public interface IContentProcessor
-	{
-		void Process(Content content);
-	}
-}
